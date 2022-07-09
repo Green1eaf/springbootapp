a8c9177aa8e1132b5f6d3b9d125e61f97d6dece4
Green1eaf <olegsmirnov86@icloud.com>
2022-07-09 15:12:47 +0300

    dp properties and create model

---
+++ src/main/java/com/green1eaf/myapp/model/Meal.java
@@ -0,0 +1,32 @@
+package com.green1eaf.myapp.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "meals")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Meal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "date_time")
+    private LocalDateTime dateTime;
+
+    @Column(name = "calories")
+    private int calories;
+
+
+}
