7924144617cadba269a774f73b40627f79362025
Green1eaf <olegsmirnov86@icloud.com>
2022-07-09 14:34:23 +0300

    initial project

---
+++ src/test/java/com/green1eaf/myapp/MyappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green1eaf.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
